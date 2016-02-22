3c4c8e851778c8b7bd8434e2c96a915a580229e0
Ricardo Borelli <ricardo.abc@gmail.com>
2016-02-22 16:29:08 -0300

    Update JSON package version

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
 	name: "JSONParserMiddleware",
 	dependencies: [
         .Package(url: "https://github.com/Zewo/Middleware.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 2)
 	]
 )
