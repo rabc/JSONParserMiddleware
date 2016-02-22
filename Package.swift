366f3c33b975fe6d9edba394f294efcdb1c8afdd
Ricardo Borelli <ricardo.abc@gmail.com>
2016-02-22 16:49:29 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
 	name: "JSONParserMiddleware",
 	dependencies: [
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 2)
 	]
 )
